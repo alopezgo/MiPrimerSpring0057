b0f29d7eb2bb7d9a368adcc1ad835566d59b70d8
alopezgo <alopezgo1991@gmail.com>
2023-09-04 21:06:15 -0300

    avance rest controllers

---
+++ src/main/java/cl/awakelab/miprimerspring0057/restcontroller/UsuarioRestController.java
@@ -0,0 +1,29 @@
+package cl.awakelab.miprimerspring0057.restcontroller;
+
+import cl.awakelab.miprimerspring0057.entity.Usuario;
+import cl.awakelab.miprimerspring0057.service.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuario")
+public class UsuarioRestController {
+     @Autowired
+    IUsuarioService objUsuarioService;
+
+     @PostMapping
+     public Usuario crearUsuario(@RequestBody Usuario usuario){
+         return objUsuarioService.crearUsuario(usuario);
+
+     }
+    @GetMapping
+     public List<Usuario> listarUsuario(){
+         return objUsuarioService.listarUsuario();
+    }
+    @GetMapping("{/id}")
+    public Usuario listarUsuarioID(@PathVariable int id){
+        return objUsuarioService.listarUsuarioID(id);
+    }
+}
