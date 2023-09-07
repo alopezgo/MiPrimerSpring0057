bfd107ab34c0c8cc7bba3716c61fb02c5cd80e51
alopezgo <alopezgo1991@gmail.com>
2023-09-06 21:13:56 -0300

    agreaga controlador inicio

--- src/main/java/cl/awakelab/miprimerspring0057/controller/InicioController.java
+++ src/main/java/cl/awakelab/miprimerspring0057/controller/InicioController.java
@@ -1,2 +1,13 @@
-package cl.awakelab.miprimerspring0057.controller;public class InicioController {
+package cl.awakelab.miprimerspring0057.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InicioController {
+
+    @GetMapping("/")
+    public String controladorInicio(){
+        return "index";
+    }
 }
