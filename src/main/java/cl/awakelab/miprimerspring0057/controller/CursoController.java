022439e092b7dbdca94bf1e06504039fef562b39
alopezgo <alopezgo1991@gmail.com>
2023-09-07 22:16:48 -0300

    avance curso controller

--- src/main/java/cl/awakelab/miprimerspring0057/controller/CursoController.java
+++ src/main/java/cl/awakelab/miprimerspring0057/controller/CursoController.java
@@ -1,4 +1,49 @@
 package cl.awakelab.miprimerspring0057.controller;
 
+import cl.awakelab.miprimerspring0057.entity.Curso;
+import cl.awakelab.miprimerspring0057.entity.Usuario;
+import cl.awakelab.miprimerspring0057.service.ICursoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/curso")
 public class CursoController {
+
+    @Autowired
+    ICursoService objCursoService;
+
+    @GetMapping
+    public String listaUsuarios(Model model){
+        List<Curso> listaCursos = objCursoService.listarCurso();
+        model.addAttribute("atributoListaCursos", listaCursos);
+
+        return "templateListarCursos";
+    }
+
+    @GetMapping("/listar/{id}")
+    public String listarCursoId(@PathVariable int id, Model model){
+        model.addAttribute("tituloCursoId", "Curso encontrado por ID");
+        Curso cursoEncontrado = objCursoService.listarCursoID(id);
+        model.addAttribute("cursoEncontrado",cursoEncontrado);
+
+        return "templateListarCursoID";
+    }
+
+    @GetMapping("/crearCurso")
+    public String formCrearCurso(){
+
+        return "templateFormCrearCurso";
+    }
+
+    @PostMapping("/crearCurso")
+    public String crearCurso(@ModelAttribute Curso curso){
+        objCursoService.crearCurso(curso);
+
+        return "redirect:/curso";
+    }
 }
